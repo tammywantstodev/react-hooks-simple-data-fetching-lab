c10061088a6c0d9c0442982f96b7b259e721a88d
billn <billnyetherussianspy679@gmail.com>
2025-01-21 17:37:33 +0300

    saving changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+export default function App() {
+    const[image,setImage] = useState(null)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {setImage(data.message)})
+    }, [])
+    if (!image) {
+        return <h2>Loading...</h2>}
+    return (
+        <img src={image} alt="Random Dog" />
+    )
+}
+//
